e8e373f61862f0b140ef11478841d286c7870561
NADKPC <150670498+NADKPC@users.noreply.github.com>
2023-11-13 17:09:08 +0100

    Update streamlit_app.py
    Add lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('my Parents new healthy Diner')
+
 streamlit.header('breakfast menu')
+streamlit.text('Meal 1')
+streamlit.text('Meal 2')
+streamlit.text('Meal 3')
