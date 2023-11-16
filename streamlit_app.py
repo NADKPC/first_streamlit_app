0cc3a28a46c9eee43e2702fcff622f5ef7fb244e
NADKPC <150670498+NADKPC@users.noreply.github.com>
2023-11-16 11:12:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,3 @@ except URLError as e:
 
 
 
-# Stop
-#streamlit.stop()
-
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
-
-
-#Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
